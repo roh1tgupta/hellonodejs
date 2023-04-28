fdce7894f59c919f9e5d1d4ac57ed50d5533ae45
Rohit Gupta <rogupta@mafcarrefour.com>
2023-04-28 12:16:52 +0530

    adding server nad packagejon

---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World! welcome to nodejs');
+}).listen(8080);
